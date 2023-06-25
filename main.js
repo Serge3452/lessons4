9dd589fe7f9bf6d405ba1f9966f07102186878e6
Serge3452 <serge3453@gmail.com>
2023-06-25 23:43:32 +0500

    Lessons_5

--- main.js
+++ main.js
@@ -1,58 +1,16 @@
-// Урок 4 
-// Домашнее задание #1:
-let a = +prompt( "Введите переменную A");
-let b = +prompt("Введите переменную B");
-let rez = a+b >=15 ? console.log("Да") : console.log("Нет"); 
-
-// Домашнее задание #2:
-let c = prompt( "Введите переменную C");
-let d = prompt("Введите переменную D");
-console.log (c===d);
-
-// Урок 4
 
 
+// Урок 5
 
-// Возведение в степень
+let i= 0;
+while (i<=10) {
+    if(i !=0){
+        console.log(i);}
+    i+=2;
+}
 
-//console.log(2**4) 
-////console.log(Math.pow(2,4));
+//let a=0
+//console.log(++a);
 
-// Остаток от деления
-//console.log(10%53)
-
-
-// Операторы сравнения: < > >= <= == = !=
-//console.log(5 != 5);
-//console.log("lets" < "qwe");
-//console.log( 1 == "1");
-//console.log(true == 1);
-//console.log(false == 0);
-//console.log(1 === "1");
-
-
-//  Логические операторы  ИЛИ || 
-//let a=0;
-//console.log(a>2 || a==1);
- // true || true || true
- // falce || true || true
-// true || false || true
-// false || false || false
-//let b=5;
-//if (b<5 || b>5|| b==5){
-//    console.log((true));
-//}
-//  Оператор И  &&
-
-// true || true || true
-// falce || true || false
-// true || false || false
-// false || false || false
-
-//let b=5;
-//if (b==5 && b>4 && b<5){
-//    console.log((true));}
-// Оператор НЕ  ! ( только с булевыми значениями)
-//console.log(! true);
-
-
+//let a=2;
+//console.log(--a);
